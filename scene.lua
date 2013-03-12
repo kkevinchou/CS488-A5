c01f01b894e62a5cfd8f292f1e9d00390013b205
Kevin C <kkevinchou@gmail.com>
2013-03-11 23:47:47 -0400

    scene files

--- scene.lua
+++ scene.lua
@@ -1,64 +1,46 @@
--- A more macho version of simple_cows.py, in which cows aren't
--- spheres, they're cow-shaped polyhedral models.
+-- A simple scene with some miscellaneous geometry.
 
+mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25)
+mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 25)
+mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25)
+mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
 
--- We'll need an extra function that knows how to read Wavefront .OBJ
--- files.
+scene_root = gr.node('root')
 
-require('readobj')
+s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
+scene_root:add_child(s1)
+s1:set_material(mat1)
 
-stone = gr.material({0.8, 0.7, 0.7}, {0.0, 0.0, 0.0}, 0)
-grass = gr.material({0.1, 0.7, 0.1}, {0.0, 0.0, 0.0}, 0)
-hide = gr.material({0.84, 0.6, 0.53}, {0.3, 0.3, 0.3}, 20)
+s2 = gr.nh_sphere('s2', {200, 50, -100}, 150)
+scene_root:add_child(s2)
+s2:set_material(mat1)
 
-cow_poly = gr.mesh('dragon', readobj('dragon.obj'))
-factor = 2.0/(2.76+3.637)
+s3 = gr.nh_sphere('s3', {0, -1200, -500}, 1000)
+scene_root:add_child(s3)
+s3:set_material(mat2)
 
-cow_poly:set_material(hide)
+b1 = gr.nh_box('b1', {-200, -125, 0}, 100)
+scene_root:add_child(b1)
+b1:set_material(mat4)
 
-cow_poly:translate(0.0, -1.0, 0.0)
-cow_poly:scale(factor, factor, factor)
-cow_poly:translate(0.0, 3.637, 0.0)
+s4 = gr.nh_sphere('s4', {-100, 25, -300}, 50)
+scene_root:add_child(s4)
+s4:set_material(mat3)
 
--- ##############################################
--- the scene
--- ##############################################
+s5 = gr.nh_sphere('s5', {0, 100, -250}, 25)
+scene_root:add_child(s5)
+s5:set_material(mat1)
 
-scene = gr.node('scene')
-scene:rotate('X', 23)
+-- A small stellated dodecahedron.
 
--- the floor
+require('smstdodeca')
 
-plane = gr.mesh('plane', {
-           { -1, 0, -1 },
-           {  1, 0, -1 },
-           {  1,  0, 1 },
-           { -1, 0, 1  }
-        }, {
-           {3, 2, 1, 0}
-        })
-scene:add_child(plane)
-plane:set_material(grass)
-plane:scale(30, 30, 30)
+steldodec:set_material(mat3)
+scene_root:add_child(steldodec)
 
--- Use the instanced cow model to place some actual cows in the scene.
--- For convenience, do this in a loop.
+white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
+orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
-cow_number = 1
-
-for _, pt in pairs({
-              {{1,1.3,14}, 20}}) do
-   cow_instance = gr.node('cow' .. tostring(cow_number))
-   scene:add_child(cow_instance)
-   cow_instance:add_child(cow_poly)
-   cow_instance:translate(unpack(pt[1]))
-   cow_instance:rotate('Y', pt[2])
-   cow_instance:scale(1.4, 1.4, 1.4)
-
-   cow_number = cow_number + 1
-end
-
-gr.render(scene,
-      'sample.png', 256, 256,
-      {0, 2, 30}, {0, 0, -1}, {0, 1, 0}, 50,
-      {0.4, 0.4, 0.4}, {gr.light({200, 202, 430}, {0.8, 0.8, 0.8}, {1, 0, 0})})
+gr.render(scene_root, 'nonhier.png', 256, 256,
+      {0, 0, 800}, {0, 0, -1}, {0, 1, 0}, 50,
+      {0.3, 0.3, 0.3}, {white_light, orange_light})
